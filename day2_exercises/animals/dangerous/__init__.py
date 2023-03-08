84e70e00952843c1421f656cce1be4fd4955c987
Marina <marina.motamerlo@icm.uu.se>
2023-03-08 16:37:54 +0100

    Deleted unnecessary intermediate files, updated init files

--- day2_exercises/animals/dangerous/__init__.py
+++ day2_exercises/animals/dangerous/__init__.py
@@ -6,7 +6,6 @@ Created on Tue Mar  7 13:22:49 2023
 @author: marmo435
 """
 
-from .dangerous import *
-#from .dmammals import Mammals
-#from .dbirds import Birds
-#from .dfish import Fish
+from .dmammals import Mammals
+from .dbirds import Birds
+from .dfish import Fish
--- day2_exercises/animals/harmless/__init__.py
+++ day2_exercises/animals/harmless/__init__.py
@@ -6,7 +6,6 @@ Created on Tue Mar  7 13:22:49 2023
 @author: marmo435
 """
 
-#from . import harmless
 from .hmammals import Mammals
 from .hbirds import Birds
 from .hfish import Fish
