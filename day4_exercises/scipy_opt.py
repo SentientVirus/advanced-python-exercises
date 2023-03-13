9396baabb5e0046a97a6269b6abde6b15e5472da
Marina <marina.motamerlo@icm.uu.se>
2023-03-13 10:42:44 +0100

    Used minimize function to make an approximation

--- day4_exercises/scipy_opt.py
+++ day4_exercises/scipy_opt.py
@@ -8,23 +8,26 @@ Created on Fri Mar 10 14:36:05 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
-#from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize
 from scipy.interpolate import interp1d
+from sklearn.metrics import mean_squared_error
 
 exp_data = np.load('I_q_IPA_exp.npy')
 model = np.load('I_q_IPA_model.npy')
 
-def scale(x, a, b):
-    scaled = a*x
-    return scaled - b
 
-exp_data = exp_data[~np.isnan(exp_data[:, 0])]
+exp_data = exp_data[~np.isnan(exp_data[:, 1])]
 
-model_x = interp1d(exp_data[:, 0], exp_data[:, 1], kind = 'nearest')
+model_x = interp1d(model[:, 0], model[:, 1])
 
-new_x = np.linspace(min(exp_data[:, 0]), max(exp_data[:, 0]), 30)
-new_y = model_x(new_x)
+new_y = model_x(exp_data[:, 0])
 
+
+new_model = minimize(mean_squared_error, exp_data[:, 1], args = (new_y,))
+
+plt.figure(figsize=(12, 8), dpi=80)
 plt.scatter(exp_data[:, 0], exp_data[:, 1])
-plt.scatter(new_x, new_y)
+plt.scatter(exp_data[:, 0], new_model.x/10**4)
+
 plt.savefig('approximation.png')
+
